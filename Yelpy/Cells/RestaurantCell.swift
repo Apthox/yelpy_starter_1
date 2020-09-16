63e0ff0a22503051372ebad0ebb2439b40da062a
Apthox <kevin@apthos.net>
2020-09-15 17:25:39 -0700

    init

---
+++ Yelpy/Cells/RestaurantCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantCell.swift
+//  Yelpy
+//
+//  Created by Kevin Guzman on 9/15/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+    @IBOutlet weak var RestaurantImage: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
